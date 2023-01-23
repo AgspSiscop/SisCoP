232f6b96e6b17d9cf10d8dd9e5196b03c7da9f1b
Cardosojl <cardosojl@outlook.com.br>
2023-01-23 14:40:11 -0300

    changed the DB models to async methods

--- config/auth.js
+++ config/auth.js
@@ -1,39 +1,6 @@
 const localStrategy =  require('passport-local').Strategy;
-const mongoose = require('mongoose');
 const bcrypt =  require('bcryptjs');
-//require('../src/models/profiles/Users');
-//const Users = mongoose.model('user');
 const Users = require('../src/models/profiles/UsersDB');
-const passport = require('passport');
-
-/*module.exports = function(passport){    
-    passport.use(new localStrategy({usernameField: 'name', passwordField: 'password'}, (name, password, done) =>{        
-        Users.findOne({name: name}).then((user) => {            
-            if(!user){
-                return done(null, false, {message: 'Esra conta não existe.'});
-            }else{
-                bcrypt.compare(password, user.password, (error, match) => {
-                    if(match){
-                        return done(null, user);
-                    }else{
-                        return done(null, false, {message: 'Senha inválida.'})
-                    }
-                });
-            }
-
-        });
-    }));
-
-    passport.serializeUser((user, done) => {
-        done(null, user._id)
-    });
-
-    passport.deserializeUser((id, done) => {        
-        Users.findById(id, (error, user) =>{
-            done(error, user);
-        })
-    });
-}*/
 
 module.exports = function(passport){
     passport.use(new localStrategy({usernameField: 'name', passwordField: 'password'},
@@ -74,9 +41,7 @@ module.exports = function(passport){
             done(error);
         }
     });
-    
-
 }
 
-//module.exports = strategy;
+
 
--- src/controller/profiles/log.js
+++ src/controller/profiles/log.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const mongoose =  require('mongoose');
-const bcrypt =  require('bcryptjs');
-//require('../../models/profiles/Users');
 const isAuth =require('../../../config/isAuth');
 const resolver =  require('../../../config/errorHandler');
-//const Users =  mongoose.model('user');
 const passport =  require('passport');
 const Users = require('../../models/profiles/UsersDB');
 
@@ -49,57 +45,7 @@ router.post('/saveregister', isAuth, resolver( async(req, res) =>{
         res.render('profiles/register', {errors: create.errors});
     }else{
         res.redirect('/register');
-    }
-    
-    /*const errors = [];
-    Users.find({name: req.body.name}).lean().then((user) =>{        
-        if(user.length == 0){
-            if(!req.body.name || req.body.name == null ){
-                errors.push({text: 'Nome inválido.'});
-            }
-            if(!req.body.password || req.body.password == null){
-                errors.push({text: 'Senha inválida.'});
-            }
-            if(!req.body.section || req.body.section == null || req.body.section == '0'){
-                errors.push({text: 'Seção inválida.'});
-            }
-            if(!req.body.level || req.body.level == null || req.body.level  < 1 && req.body.level > 5){
-                errors.push({text: 'Level inválido.'});
-            }
-            if(errors.length > 0){
-                res.render('profiles/register', {errors: errors});
-            }else{
-                const newUser = {
-                    name: req.body.name,
-                    password: req.body.password,
-                    pg: req.body.pg,
-                    section: req.body.section,
-                    level: req.body.level
-                };
-                bcrypt.genSalt(10, (error, salt) => {
-                    bcrypt.hash(newUser.password, salt, (error, hash) =>{
-                        if(error){                            
-                            console.log(error);
-                            res.render('error/error', {serverError: {message: `Houve um erro ao Salvar as informações ${error.message}`, code: error.code}});
-                        }else{
-                            newUser.password = hash
-                            new Users(newUser).save().then(() => {
-                                res.redirect('/register');
-                            }).catch((error) => {
-                                console.log(error);
-                                res.render('error/error', {serverError: {message: error.message, code: error.code}});
-                            });
-                        }
-                    });
-                });               
-            }            
-        }else{            
-            res.render('error/error', {serverError: {message: `Usuário Já existe.`}});
-        }
-    }).catch((error) => {
-        console.log(error);
-        res.render('error/error', {serverError: {message: error.message, code: error.code}});
-    });*/
+    }   
 }));
 
 router.get('/updateuser', isAuth ,resolver((req, res) => {
@@ -109,14 +55,7 @@ router.get('/updateuser', isAuth ,resolver((req, res) => {
 router.post('/getuser', isAuth, resolver( async(req,res) => {
     const user = new Users(req.body, res.locals, req.params);
     const userObj = await user.findOneByParam({name: req.body.name});
-    res.send(JSON.stringify(userObj));
-    
-    /*Users.findOne({name: req.body.name}).lean().then((user) => {
-        res.send(JSON.stringify(user));
-    }).catch((error) => {
-        console.log(error);
-        res.render('error/error', {serverError: {message: error.message, code: error.code}});
-    });*/
+    res.send(JSON.stringify(userObj));    
 }));
 
 router.post('/updateuser/update', isAuth, resolver( async(req, res) => {
@@ -126,79 +65,13 @@ router.post('/updateuser/update', isAuth, resolver( async(req, res) => {
         res.render('profiles/updateprofile', {errors: update.errors});
     }else{
         res.redirect('/updateuser');
-    }
-    /*const errors = [];
-    Users.find({name: req.body.name}).lean().then((user) =>{        
-        if((user.length == 0) || (user.length != 0 && req.body.name == req.body.userselected)){
-            if(!req.body.name || req.body.name == null ){
-                errors.push({text: 'Nome inválido.'});
-            }           
-            if(!req.body.level || req.body.level == null || req.body.level  < 1 && req.body.level > 5){
-                errors.push({text: 'Level inválido.'});
-            }
-            if(errors.length > 0){
-                res.render('profiles/updateprofile', {errors: errors});
-            }else{
-                if(req.body.password){
-                    const User = {
-                        name: req.body.name,
-                        password: req.body.password,
-                        pg: req.body.pg,
-                        section: req.body.section,
-                        level: req.body.level
-                    };
-                    bcrypt.genSalt(10, (error, salt) => {
-                        bcrypt.hash(User.password, salt, (error, hash) =>{
-                            if(error){                            
-                                console.log(error);
-                                res.render('error/error', {serverError: {message: `Houve um erro ao Salvar as informações ${error.message}`, code: error.code}});
-                            }else{
-                                User.password = hash
-                                Users.updateOne({name: req.body.userselected}, {$set: User}).then(() => {
-                                    res.redirect('/updateuser');
-                                }).catch((error) => {
-                                    console.log(error);
-                                    res.render('error/error', {serverError: {message: error.message, code: error.code}});
-                                });
-                            }
-                        });
-                    });
-                }else{
-                    const User = {
-                        name: req.body.name,                        
-                        pg: req.body.pg,
-                        section: req.body.section,
-                        level: req.body.level
-                    };
-                    Users.updateOne(
-                        {name: req.body.userselected}, {$set: User}).then(() => {            
-                            res.redirect('/updateuser');
-                        }).catch((error) => {
-                            console.log(error);
-                            res.render('error/error', {serverError: {message: error.message, code: error.code}});
-                        });
-                }
-            }            
-        }else{            
-            res.render('error/error', {serverError: {message: `Usuário Já existe.`}});
-        }
-    }).catch((error) => {
-        console.log(error);
-        res.render('error/error', {serverError: {message: error.message, code: error.code}});
-    });*/
+    }   
 }));
 
 router.get('/profile/:name', isAuth, resolver( async(req, res) => {
     const user = new Users(req.body, res.locals, req.params);
     const userObj = await user.findOneByParam({name: req.params.name});
-    res.render('profiles/profiledescription', {paramName: req.params.name, profile: userObj});
-    
-    /*Users.findOne({name: req.params.name}).lean().then((user) => {
-        res.render('profiles/profiledescription', {paramName: req.params.name, profile: user});
-    }).catch((error) => {
-        console.log(error);
-        res.render('error/error', {serverError: {message: error.message, code: error.code}});
-    })*/
+    res.render('profiles/profiledescription', {paramName: req.params.name, profile: userObj});   
 }))
 
 router.get('/deleteuser', isAuth, resolver((req, res) => {
@@ -208,14 +81,7 @@ router.get('/deleteuser', isAuth, resolver((req, res) => {
 router.post('/deleteuser/delete', isAuth, resolver( async(req, res) => {
     const user = new Users(req.body, res.locals, req.params);
     await user.deleteOne();
-    res.redirect('/deleteuser');
-    
-    /*Users.deleteOne({name: req.body.name}).then(() => {
-        res.redirect('/deleteuser');
-    }).catch((error) => {
-        console.log(error);
-        res.render('error/error', {serverError: {message: error.message, code: error.code}})
-    })*/
+    res.redirect('/deleteuser');    
 }));
 
 router.post('/changedatas', isAuth, resolver( async(req, res) => {    
